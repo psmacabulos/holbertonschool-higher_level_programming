2b1ed9ec78cea03c5e95cf0703e713b8f628f972
psmacabulos <patrick.macabulos@gmail.com>
2026-01-30 14:48:50 +1100

    fix: remove unnecessary character during slicing

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
 print(
-    f"{str[0:10]} {str[10:]}{str[0:10]} {str[10:]}"
-    f"{str[0:10]} {str[10:]}\n{str[0:10]}")
+    f"{str[:10]} {str[10:]}{str[:10]} {str[10:]}"
+    f"{str[:10]} {str[10:]}\n{str[:10]}")
