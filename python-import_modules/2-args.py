06eb1d500b0cc387ee094008772077ccd510965e
psmacabulos <patrick.macabulos@gmail.com>
2026-02-01 05:09:26 +1100

    fix: add the missing "s" when there is no argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
     arg_count = len(sys.argv)
     if arg_count == 1:
-        print("{} argument.".format(arg_count - 1))
+        print("{} arguments.".format(arg_count - 1))
     elif arg_count == 2:
         print("{} argument:".format(arg_count - 1))
     else:
