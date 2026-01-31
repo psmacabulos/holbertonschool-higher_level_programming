ec0b43fa83e66f3e166cac0346bebb3d69d70386
psmacabulos <patrick.macabulos@gmail.com>
2026-02-01 05:08:22 +1100

    Implement 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    arg_count = len(sys.argv)
+    if arg_count == 1:
+        print("{} argument.".format(arg_count - 1))
+    elif arg_count == 2:
+        print("{} argument:".format(arg_count - 1))
+    else:
+        print("{} arguments:".format(arg_count - 1))
+
+    for i in range(1, arg_count):
+        print("{}: {}".format(i, sys.argv[i]))
