b5afd524ea46a54717a82c0b0ce8faa10b3f4b0f
psmacabulos <patrick.macabulos@gmail.com>
2026-02-01 05:18:56 +1100

    Implement 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    arg_count = len(sys.argv) - 1
+    sum = 0
+
+    for i in range(1, arg_count + 1):
+        sum += int(sys.argv[i])
+    print(sum)
