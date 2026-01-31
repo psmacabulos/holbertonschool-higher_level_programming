a4bbf856a68817a99f1b307d981755c52becc37c
psmacabulos <patrick.macabulos@gmail.com>
2026-01-31 13:42:56 +1100

    Implement 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        newascii = ord(str[i])
+        if not (newascii >= 97 and newascii <= 122):
+            uppercaseLetter = str[i]
+        else:
+            uppercaseLetter = chr(newascii - 32)
+        if i == len(str) - 1:
+            print("{}".format(uppercaseLetter))
+            break
+        print("{}".format(uppercaseLetter), end="")
