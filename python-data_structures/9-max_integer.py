67e77aa81b9262be07adfcc03a18c9547f8749c6
psmacabulos <patrick.macabulos@gmail.com>
2026-02-02 22:26:45 +1100

    fix: refactor to cater if all elements are below 0

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
     if not my_list:
         return (None)
+    max = my_list[0]
     for num in my_list:
         if num > max:
             max = num
