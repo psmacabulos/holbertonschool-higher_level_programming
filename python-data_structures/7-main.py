57d85f89e7dc6fec44355157634e39d6637bc764
psmacabulos <patrick.macabulos@gmail.com>
2026-02-02 22:08:34 +1100

    Implement 7-add_tuple.py

--- python-data_structures/7-main.py
+++ python-data_structures/7-main.py
@@ -4,5 +4,8 @@ add_tuple = __import__("7-add_tuple").add_tuple
 
 tuple_a = (1, 89)
 tuple_b = (88, 11)
-print(add_tuple(tuple_a, tuple_b))
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
 
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
