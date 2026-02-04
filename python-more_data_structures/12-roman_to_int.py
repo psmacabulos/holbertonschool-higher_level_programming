e6e82d88faa6a59f44c1639d0d0170420ad43f86
psmacabulos <patrick.macabulos@gmail.com>
2026-02-04 23:47:44 +1100

    Implement 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 
-
 def roman_to_int(roman_string):
-    if not roman_string or isinstance(roman_string, str):
+    if not roman_string or not isinstance(roman_string, str):
         return 0
-    roman_chart = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C':100, 'D':500, 'M':1000 }
-    value = 0;
-    
+    roman_chart = {
+        'I': 1, 'V': 5,
+        'X': 10, 'L': 50,
+        'C': 100, 'D': 500,
+        'M': 1000
+    }
+    value = 0
+    prev_val = 0
+    counter = 0
     for s in roman_string:
-       value += roman_chart[s]
+
+        if counter > 0 and prev_val < roman_chart[s]:
+            value = roman_chart[s] - prev_val
+        else:
+            value += roman_chart[s]
+        prev_val = value
+        counter += 1
+
+    return value
