ad39ffcba260b0b70287d02eaec94df13390dfd2
psmacabulos <patrick.macabulos@gmail.com>
2026-02-04 23:23:14 +1100

    Implement 11-multiply_list_map.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+
+def roman_to_int(roman_string):
+    if not roman_string or isinstance(roman_string, str):
+        return 0
+    roman_chart = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C':100, 'D':500, 'M':1000 }
+    value = 0;
+    
+    for s in roman_string:
+       value += roman_chart[s]
