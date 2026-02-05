e26a22eb356ddb3b4c5ef8ea4aa6b9f11f29b20e
psmacabulos <patrick.macabulos@gmail.com>
2026-02-05 15:58:46 +1100

    Implement 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    try:
+        for i in range(x):
+            print(f"{my_list[i]}", end="")
+            printed += 1
+        print()
+    except ValueError:
+        print("Wrong data type")
+    except IndexError:
+        print()
+    return printed
