fe5a26b0061d6df55fc3063f3acb5b672d04598c
psmacabulos <patrick.macabulos@gmail.com>
2026-02-15 00:16:34 +1100

    Implement 6-raise_exception_msg.py

--- python-exceptions/6-main.py
+++ python-exceptions/6-main.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-# Test file provided by Holberton
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
