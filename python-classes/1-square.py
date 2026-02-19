ff71009d2c738788e1540cfa9058a37d1069e628
psmacabulos <patrick.macabulos@gmail.com>
2026-02-19 14:04:24 +1100

    Implement 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Module that defines a Square with a private size attribute."""
 
+
 class Square:
     """Defines a square."""
 
     def __init__(self, size):
         """Initialize square with size."""
-        pass
+        self.__size = size
