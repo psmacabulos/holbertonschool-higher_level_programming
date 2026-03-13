9d7e7589defc6b15908cc4870f5be5a4280041e7
psmacabulos <patrick.macabulos@gmail.com>
2026-03-13 17:53:37 +1100

    Implement 5-square.py

--- python-classes/5-main.py
+++ python-classes/5-main.py
@@ -2,6 +2,16 @@
 Square = __import__('5-square').Square
 
 my_square = Square(3)
-print(my_square.size)
-my_square.size = 6
-print(my_square.size)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
-"""Module that defines a Square with getter and setter."""
+"""Module that defines a Square class"""
+
 
 class Square:
-    """Defines a square with validated size property."""
+    """Defines a square"""
 
     def __init__(self, size=0):
-        """Initialize square."""
-        pass
+        """Initialize square with optional size"""
+        self.size = size
 
     @property
     def size(self):
-        """Retrieve size."""
-        pass
+        """Retrieve size"""
+        return self.__size
 
     @size.setter
-    def size(self, value):
-        """Set size with validation."""
-        pass
+    def size(self, size):
+        """Set size with validation"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
 
     def area(self):
-        """Return area."""
-        pass
+        """Return the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        for row in range(self.__size):
+            for col in range(self.__size):
+                print("#", end='')
+            print()
