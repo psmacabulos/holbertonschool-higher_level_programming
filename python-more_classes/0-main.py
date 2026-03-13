9fed24a1e007fd8b30cd5013ad6fda05ebac6ff5
psmacabulos <patrick.macabulos@gmail.com>
2026-03-13 22:11:44 +1100

    Implement 0-rectangle.py

--- python-more_classes/0-main.py
+++ python-more_classes/0-main.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-
-"""
-Test file for task 0
-"""
-
 Rectangle = __import__('0-rectangle').Rectangle
 
-# Example scaffold tests
-if __name__ == "__main__":
-    r = Rectangle()
-
-    print("Rectangle created")
-    print("Area:", r.area())
-    print("Perimeter:", r.perimeter())
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
