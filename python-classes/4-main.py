0e938b8d79e03e467c927c4d077b1ca7fc05f285
psmacabulos <patrick.macabulos@gmail.com>
2026-03-13 17:22:39 +1100

    Implement 4-square.py

--- python-classes/4-main.py
+++ python-classes/4-main.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 Square = __import__('4-square').Square
 
-my_square = Square(2)
-print(my_square.size)
-print(my_square.area())
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
+
+try:
+    new = Square(5)
+    print("Area: {} for size: {}".format(new.area(), new.size))
+    new.__size = -100
+    print("Area: {} for size: {}".format(new.area(), new.size))
+except Exception as e:
+    print(e)
+
+print(new.__dict__)
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""Module that defines a Square with size property."""
-
 class Square:
-    """Defines a square with size getter."""
+    """Defines a square"""
 
     def __init__(self, size=0):
-        """Initialize square."""
-        pass
+        """Initialize square with optional size"""
+        self.size = size
 
     @property
     def size(self):
-        """Retrieve the size of the square."""
-        pass
+        """Retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
 
     def area(self):
-        """Return the area of the square."""
-        pass
+        """Return the area of the square"""
+        return self.__size ** 2
