98263d0d8cddefe0c0e84fa28784515511f5c432
psmacabulos <patrick.macabulos@gmail.com>
2026-03-16 21:07:28 +1100

    Implement 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Module about inheritance"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if the object is exactly an instance of the class"""
+    return type(obj) is a_class
