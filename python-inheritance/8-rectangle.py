544f7a776c289c41eb02635f9b6b59c84f0e41aa
psmacabulos <patrick.macabulos@gmail.com>
2026-03-16 22:56:34 +1100

    docs: add docstring on method and module

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""Module for Rectangle"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""Module about BaseGeometry"""
 
 
 class Rectangle(BaseGeometry):
-    """SubClass Rectangle from Base Geometry"""
+    """Rectangle class that inherits from BaseGeometry"""
+
     def __init__(self, width, height):
-        super().__init__()
+        """Initialize rectangle"""
 
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+
         self.__width = width
         self.__height = height
