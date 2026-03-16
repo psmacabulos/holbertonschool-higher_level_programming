794cf512df20ae19caf565fabf88c5434dbe8a57
psmacabulos <patrick.macabulos@gmail.com>
2026-03-16 21:23:50 +1100

    Implement 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Module about inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if the object is an instance of a class
+    that inherited from the specified class"""
+
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
--- python-inheritance/4-main.py
+++ python-inheritance/4-main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-# Test file for 4-inherits_from.py
+inherits_from = __import__('4-inherits_from').inherits_from
 
-if __name__ == "__main__":
-    print("Running test for 4-inherits_from.py")
+a = True
+if inherits_from(a, int):
+    print("{} inherited from class {}".format(a, int.__name__))
+if inherits_from(a, bool):
+    print("{} inherited from class {}".format(a, bool.__name__))
+if inherits_from(a, object):
+    print("{} inherited from class {}".format(a, object.__name__))
