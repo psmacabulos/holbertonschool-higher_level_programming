8f635ee3660d877d516ea3dc719ff31e99c6c96c
psmacabulos <patrick.macabulos@gmail.com>
2026-03-16 21:39:42 +1100

    Implement 7-base_geometry.py

--- python-inheritance/7-main.py
+++ python-inheritance/7-main.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
-# Test file for 7-base_geometry.py
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-if __name__ == "__main__":
-    print("Running test for 7-base_geometry.py")
+bg = BaseGeometry()
+
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
+
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
