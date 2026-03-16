5a0962e2a2da96eadefce56d361d4b1886c714d1
psmacabulos <patrick.macabulos@gmail.com>
2026-03-16 15:35:02 +1100

    Implement 9-rectangle.py

--- python-more_classes/9-main.py
+++ python-more_classes/9-main.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-
-"""
-Test file for task 9
-"""
-
 Rectangle = __import__('9-rectangle').Rectangle
 
-# Example scaffold tests
-if __name__ == "__main__":
-    r = Rectangle()
-
-    print("Rectangle created")
-    print("Area:", r.area())
-    print("Perimeter:", r.perimeter())
+my_square = Rectangle.square(5)
+print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
+print(my_square)
