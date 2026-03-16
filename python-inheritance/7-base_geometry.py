416cdc395735441e13297cc9416d669e490d45b3
psmacabulos <patrick.macabulos@gmail.com>
2026-03-16 22:29:36 +1100

    fix: use type instead of is instance in integer validator to cover edge cases

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Module about inheritance"""
+"""Module about BaseGeometry"""
 
 
 class BaseGeometry:
-    """Blank class"""
+    """BaseGeometry class"""
+
     def area(self):
+        """area is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        """validate integer value"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
