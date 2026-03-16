2930c1bde775e25716cf744c8046556b40b8656e
psmacabulos <patrick.macabulos@gmail.com>
2026-03-16 21:12:17 +1100

    Implement 3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Module about inheritance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if the object is exactly an instance of the class"""
+    return isinstance(obj, a_class)
--- python-inheritance/3-main.py
+++ python-inheritance/3-main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-# Test file for 3-is_kind_of_class.py
+is_kind_of_class = __import__('3-is_kind_of_class').is_kind_of_class
 
-if __name__ == "__main__":
-    print("Running test for 3-is_kind_of_class.py")
+a = 1
+if is_kind_of_class(a, int):
+    print("{} comes from {}".format(a, int.__name__))
+if is_kind_of_class(a, float):
+    print("{} comes from {}".format(a, float.__name__))
+if is_kind_of_class(a, object):
+    print("{} comes from {}".format(a, object.__name__))
