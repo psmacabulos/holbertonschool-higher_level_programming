1f132be35a27080fb7aa472b320492cf491959a3
psmacabulos <patrick.macabulos@gmail.com>
2026-03-17 20:03:26 +1100

    Implement task 05

--- python-abc/5-main.py
+++ python-abc/5-main.py
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+from task_05_dragon import Dragon
+
+dragon = Dragon()
+dragon.swim()  # Outputs: The creature swims!
+dragon.fly()   # Outputs: The creature flies!
+dragon.roar()  # Outputs: The dragon roars!
--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Module for Dragon using mixins"""
+
+
+class SwimMixin:
+    """Mixin for swimming capability"""
+
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    """Mixin for flying capability"""
+
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """Dragon class combining multiple behaviors"""
+
+    def roar(self):
+        print("The dragon roars!")
