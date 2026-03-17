3dd87df576424f7ba89c918147cc10b0a7a7850d
psmacabulos <patrick.macabulos@gmail.com>
2026-03-17 21:13:48 +1100

    docs: add module and function docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""This module is about reading file in Python"""
+
 
 def read_file(filename=""):
+    """Read file function"""
+
     with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
         print(content)
