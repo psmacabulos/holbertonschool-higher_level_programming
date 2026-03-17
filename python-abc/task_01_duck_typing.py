709c927d9261bd4072e4c7887bd40dc60c69793f
psmacabulos <patrick.macabulos@gmail.com>
2026-03-17 18:33:11 +1100

    fix: make the atrributes of Circle and Rectangle private

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -21,27 +21,27 @@ class Circle(Shape):
     """SubClass Circle"""
 
     def __init__(self, radius):
-        self.radius = radius
+        self.__radius = radius
 
     def area(self):
-        return math.pi * (self.radius ** 2)
+        return math.pi * (self.__radius ** 2)
 
     def perimeter(self):
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * self.__radius
 
 
 class Rectangle(Shape):
     """SubClass Rectangle"""
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        return 2 * (self.__width + self.__height)
 
 
 def shape_info(arg):
