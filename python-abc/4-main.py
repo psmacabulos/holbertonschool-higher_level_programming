47ef042c410e1325408cb02f1c9cf8404d4dffb0
psmacabulos <patrick.macabulos@gmail.com>
2026-03-17 19:58:51 +1100

    Implement task 04

--- python-abc/4-main.py
+++ python-abc/4-main.py
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+from task_04_flyingfish import Fish, FlyingFish
+
+flying_fish = FlyingFish()
+flying_fish.swim()
+flying_fish.fly()
+flying_fish.habitat()
