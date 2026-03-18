7195c25363a42cd903307b43d57d0bb9e225f566
psmacabulos <patrick.macabulos@gmail.com>
2026-03-18 23:15:49 +1100

    Implement 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for reading arguments"""
+
+import sys
+import json
+
+save_to_json = __import__("5-save_to_json_file").save_to_json_file
+load_from_json = __import__("6-load_from_json_file").load_from_json_file
+
+
+def load_from_json_file(filename):
+    """Method to add the sys argument to file"""
+    my_list = load_from_json(filename)
+    my_list.extend(sys.argv[1:])
+    save_to_json(my_list, filename)
