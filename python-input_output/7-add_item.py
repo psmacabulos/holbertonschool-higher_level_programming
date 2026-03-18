33b04bb04b29ef582b5ecf80db9a270e2665a8f4
psmacabulos <patrick.macabulos@gmail.com>
2026-03-18 23:18:35 +1100

    fix: add scenario for empty file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,5 +11,8 @@ load_from_json = __import__("6-load_from_json_file").load_from_json_file
 def load_from_json_file(filename):
     """Method to add the sys argument to file"""
     my_list = load_from_json(filename)
-    my_list.extend(sys.argv[1:])
+    if my_list == []:
+        my_list.append(sys.argv[1:])
+    else:
+        my_list.extend(sys.argv[1:])
     save_to_json(my_list, filename)
