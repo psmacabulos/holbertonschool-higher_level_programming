6c4a49b0c4642919cae278e018691e38d472fb58
psmacabulos <patrick.macabulos@gmail.com>
2026-03-18 23:24:30 +1100

    fix: rewrite code, have read wrong instructions

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Module for reading arguments"""
+"""Script to add items to a JSON file"""
 
 import sys
-import json
 
 save_to_json = __import__("5-save_to_json_file").save_to_json_file
 load_from_json = __import__("6-load_from_json_file").load_from_json_file
 
+filename = "add_item.json"
 
-def load_from_json_file(filename):
-    """Method to add the sys argument to file"""
-    try:
-        my_list = load_from_json(filename)
-    except FileNotFoundError:
-        my_list = []
-    my_list.extend(sys.argv[1:])
-    save_to_json(my_list, filename)
+try:
+    my_list = load_from_json(filename)
+except FileNotFoundError:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json(my_list, filename)
