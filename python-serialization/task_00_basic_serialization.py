7cb581c31299b6eb46f661b0fadc39df026b2a20
psmacabulos <patrick.macabulos@gmail.com>
2026-03-20 15:25:37 +1100

    Implement task 0 - basic serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-"""Module docstring"""
-
-def main():
-    """Main function"""
-    pass
+"""Basic serialization module"""
+import json
 
 
-if __name__ == "__main__":
-    main()
+def serialize_and_save_to_file(data, filename):
+    # Your code here to serialize and save data to the specified file
+    with open(filename, "w", encoding='utf-8') as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    # Your code here to load and deserialize data from the specified file
+    with open(filename, "r", encoding='utf-8') as f:
+        return json.load(f)
