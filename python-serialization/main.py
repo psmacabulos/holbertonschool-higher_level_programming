1efd6bcda529c41dcc38e1e340d1f94b7844d53f
psmacabulos <patrick.macabulos@gmail.com>
2026-03-20 15:53:26 +1100

    Implement task 01 - Pickling Custom Classes

--- python-serialization/main.py
+++ python-serialization/main.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python3
-from task_00_basic_serialization import load_and_deserialize, serialize_and_save_to_file
+from task_01_pickle import CustomObject
 
-# Sample data to be serialized
-data_to_serialize = {
-    "name": "John Doe",
-    "age": 30,
-    "city": "New York"
-}
+# Create an instance of CustomObject
+obj = CustomObject(name="John", age=25, is_student=True)
+print("Original Object:")
+obj.display()
 
-# Serialize the data to JSON and save it to a file
-serialize_and_save_to_file(data_to_serialize, 'data.json')
+# Serialize the object
+obj.serialize("object.pkl")
 
-# Output: The data has been serialized and saved to 'data.json'
-print("Data serialized and saved to 'data.json'.")
-
-# Load and deserialize data from 'data.json'
-deserialized_data = load_and_deserialize('data.json')
-
-# Output: The deserialized data
-print("Deserialized Data:")
-print(deserialized_data)
+# Deserialize the object into a new instance
+new_obj = CustomObject.deserialize("object.pkl")
+print("\nDeserialized Object:")
+new_obj.display()
--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python3
-"""Module docstring"""
+"""Module for Basic Pickling"""
 
-def main():
-    """Main function"""
-    pass
+import pickle
 
 
-if __name__ == "__main__":
-    main()
+class CustomObject:
+    """Custom Object class to serialize"""
+
+    def __init__(self, name, age, is_student):
+        if not isinstance(name, str):
+            raise TypeError("name must be a string")
+        self.name = name
+        if not isinstance(age, int):
+            raise TypeError("age must be an integer")
+        self.age = age
+        if not isinstance(is_student, bool):
+            raise TypeError("is_student must be a boolean")
+
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize object to a file"""
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize object from file"""
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            return None
