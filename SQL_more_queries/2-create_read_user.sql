3d6f8a75260e60fd073731d21e002a031aca4d27
psmacabulos <patrick.macabulos@gmail.com>
2026-03-30 20:06:45 +1100

    fix: correct syntax and remove quotes on database name

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,10 @@
 -- Read User
 
 -- Create the database
-CREATE IF NOT EXISTS DATABASE 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create the user
-CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant access
-GRANT SELECT PRIVILEGES ON 'hbtn_0d_2' TO 'user_0d)2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
