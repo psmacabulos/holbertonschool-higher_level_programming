3d87f19a847cd6359c248a6afa28c055dce3ec84
psmacabulos <patrick.macabulos@gmail.com>
2026-03-30 20:31:54 +1100

    fix: replace auto increment by unique

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -2,6 +2,6 @@
 
 -- Create a table with unique id
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT NOT NULL DEFAULT 1 AUTO_INCREMENT,
+    id INT NOT NULL DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
