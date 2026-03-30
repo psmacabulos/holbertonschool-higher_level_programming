78aae6386e35eb97124e7eb6c7c2b608b9693128
psmacabulos <patrick.macabulos@gmail.com>
2026-03-30 22:09:28 +1100

    Implement 10-genre_id_by_show.sql

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Genre id by show
+
+-- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title, tv_shows_genres.genre.id
+FROM tv_shows
+JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
+ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
