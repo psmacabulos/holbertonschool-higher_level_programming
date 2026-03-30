3719d7ac3cee8ebe562d113d4de0bb16769fbb60
psmacabulos <patrick.macabulos@gmail.com>
2026-03-30 17:29:28 +1100

    fix: revert to 2 lines but added comment on each query

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
 --  list priveleges for specific users
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'locahost';
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
