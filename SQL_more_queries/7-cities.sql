2c1d938e84c8d97f656efdc8f9b3bf5992be8bcd
psmacabulos <patrick.macabulos@gmail.com>
2026-03-30 20:45:00 +1100

    fix: Use the database and add name

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,9 +3,13 @@
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Use the database
+USE hbtn_0d_usa;
+
 -- Create the table
 CREATE TABLE IF NOT EXISTS cities (
     id INT PRIMARY KEY NOT NULL UNIQUE,
     state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
