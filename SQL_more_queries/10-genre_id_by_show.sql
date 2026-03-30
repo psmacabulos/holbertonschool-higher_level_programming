6a4f0a713f921e83455906a15b2f8053a8b99d31
psmacabulos <patrick.macabulos@gmail.com>
2026-03-30 22:14:00 +1100

    fix: correct table name from tv_shows_genre to tv_show_genre

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,8 @@
 -- Genre id by show
 
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_shows.title, tv_shows_genres.genre.id
+SELECT tv_shows.title, tv_show_genres.genre.id
 FROM tv_shows
-JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
-ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
+JOIN tv_shows_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
