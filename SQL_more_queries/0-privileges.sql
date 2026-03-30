03c9d3671c63f6059b23068d18d541cd844f6915
psmacabulos <patrick.macabulos@gmail.com>
2026-03-30 17:24:21 +1100

    fix: combine two query into one.

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 --  list priveleges for specific users
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'locahost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'locahost';
