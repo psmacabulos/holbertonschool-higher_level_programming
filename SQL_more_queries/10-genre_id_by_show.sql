305a2a2a0227e4503c8510f7671b4a8706229c7d
psmacabulos <patrick.macabulos@gmail.com>
2026-03-30 22:19:15 +1100

    refactor: make proper indentation for easy reading

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,8 @@
--- Genre id by show
+-- 10-genre_id_by_show.sql
 
--- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Create the query to show the requirements
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_shows_genres
-ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
