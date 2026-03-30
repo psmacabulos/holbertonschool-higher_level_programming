7eafa42383c4006549308f7f874207ececb32d90
psmacabulos <patrick.macabulos@gmail.com>
2026-03-30 19:58:46 +1100

    Implement 1-create_user.sql

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Read User
+
+-- Create the database
+CREATE IF NOT EXISTS DATABASE 'hbtn_0d_2';
+
+-- Create the user
+CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant access
+GRANT SELECT PRIVILEGES IN 'hbtn_0d_2' TO 'user_0d)2'@'localhost';
