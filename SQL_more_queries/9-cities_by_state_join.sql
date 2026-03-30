7a7384ea2f5685d468a657ca933ced49f0df11a4
psmacabulos <patrick.macabulos@gmail.com>
2026-03-30 21:43:26 +1100

    Implement 9-cities_by_states.sql

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Cities by States
+
+-- Create a script that lists all cities in the database
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN  states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
