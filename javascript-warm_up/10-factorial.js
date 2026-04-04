aca09f56e82780a2f71990fc00fbf18bc5ea8ac1
psmacabulos <patrick.macabulos@gmail.com>
2026-04-04 23:48:20 +1100

    Implement task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
 
+const number = +process.argv[2];
+
+if (Number.isNaN(number)) {
+  console.log(1);
+} else {
+  console.log(factorial(number));
+}
+
+function factorial (number) {
+  // base case
+  if ((number === 0) || (number === 1)) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
