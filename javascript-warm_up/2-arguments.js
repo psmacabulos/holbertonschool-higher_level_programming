bfc86d18e4ea9a34ccd542f8ebe6627e1dfe268a
psmacabulos <patrick.macabulos@gmail.com>
2026-04-04 20:49:53 +1100

    fix: change == to ===

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const { argv } = require('node:process');
 
 if (argv.length < 3) {
   console.log('No argument');
-} else if (argv.length == 3) {
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
