1e257c5079164f024ca96e1ad3d90a2d98e37d1b
psmacabulos <patrick.macabulos@gmail.com>
2026-04-04 23:09:33 +1100

    Implement task 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,2 +1,13 @@
 #!/usr/bin/node
 
+const first = +process.argv[2];
+const second = +process.argv[3];
+
+function addNumbers (a, b) {
+  if (Number.isNaN(a) || Number.isNaN(b)) {
+    console.log('NaN');
+    return;
+  }
+  console.log(a + b);
+}
+addNumbers(first, second);
