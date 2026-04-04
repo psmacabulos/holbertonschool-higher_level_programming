9e5d3309095395ed9622935cc6005eb9809059d2
psmacabulos <patrick.macabulos@gmail.com>
2026-04-04 21:07:28 +1100

    Implement task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
 
+const { argv } = require('node:process');
+
+const first = +argv[2];
+
+if (!Number.isNaN(first)) {
+  console.log('My number: ' + first);
+} else {
+  console.log('Not a number');
+}
