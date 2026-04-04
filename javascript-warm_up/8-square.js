b98b9457a2f435d0c905e21e68bcda02a044be91
psmacabulos <patrick.macabulos@gmail.com>
2026-04-04 22:17:47 +1100

    Implement task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
 
+const myArgs = +process.argv[2];
+
+if (!Number.isNaN(myArgs)) {
+  for (let row = 0; row < myArgs; row++) {
+    let square = '';
+    for (let col = 0; col < myArgs; col++) {
+      square += 'X';
+    }
+    console.log(square);
+  }
+} else {
+  console.log('Missing size');
+}
