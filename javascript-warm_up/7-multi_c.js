c37fee0f7f0ca149a232f6515e4f75efdc2bddba
psmacabulos <patrick.macabulos@gmail.com>
2026-04-04 21:55:36 +1100

    Implement task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,2 +1,13 @@
 #!/usr/bin/node
 
+const { argv } = require('node:process');
+
+const arg = +argv[2];
+
+if (!Number.isNaN(arg)) {
+  for (let i = 0; i < arg; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
