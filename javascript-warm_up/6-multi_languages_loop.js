5346eebe5c782a6a1172f0b2a7daaa665d6c5489
psmacabulos <patrick.macabulos@gmail.com>
2026-04-04 21:49:29 +1100

    Implement task 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
 
+const languangeArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < languangeArray.length; i++) {
+  console.log(languangeArray[i]);
+}
