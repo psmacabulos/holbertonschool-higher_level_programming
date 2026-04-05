75455d372585448f926377805acbf17c5578829a
psmacabulos <patrick.macabulos@gmail.com>
2026-04-05 11:49:53 +1000

    Implement task 6

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -5,5 +5,20 @@
  */
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Your code here
+  const URL = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+  const character = document.querySelector('#character');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const data = await response.json();
+      character.textContent = data.name;
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  fetchData();
 });
