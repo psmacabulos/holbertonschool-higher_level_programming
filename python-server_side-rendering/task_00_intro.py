722d3703ed16122a5c08f7e842d9fdd4a7c2e0d5
psmacabulos <patrick.macabulos@gmail.com>
2026-04-05 22:12:26 +1000

    Implement task 00

---
+++ python-server_side-rendering/task_00_intro.py
@@ -0,0 +1,44 @@
+"""Module to replace placeholder from an object"""
+
+
+def generate_invitations(template, attendees):
+    """Function to create invitations"""
+    # Verify the input and exit gracefully
+    if not isinstance(template, str):
+        raise TypeError("Template must be a string")
+        return
+
+    if not isinstance(attendees, list):
+        raise TypeError("Attendess must be a list")
+        return
+
+    if not all(isinstance(a, dict) for a in attendees):
+        raise TypeError("Attendees must be a list of dictionaries")
+        return
+    if template == '':
+        raise TypeError("Template is empty, no output files generated.")
+        return
+    if not attendees:
+        raise TypeError("No data provided, no output files generated.")
+        return
+
+    # Process attendees
+
+    for i, person in enumerate(attendees, start=1):
+        # Replace the placeholders with the correct value while looping
+        output = template.replace("{name}",
+                                  str(person.get("name") or "N/A"))
+        output = output.replace("{event_title}",
+                                str(person.get("event_title") or "N/A"))
+        output = output.replace("{event_date}",
+                                str(person.get("event_date") or "N/A"))
+        output = output.replace("{event_location}",
+                                str(person.get("event_location") or "N/A"))
+
+        # Create the file
+        filename = f"output_{i}.txt"
+        try:
+            with open(filename, "w", encoding='utf-8') as f:
+                f.write(output)
+        except Exception as e:
+            print(f"Error writing file {filename}: {e}")
