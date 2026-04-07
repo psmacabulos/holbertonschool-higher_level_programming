177c05743aead4bd4b6b37ae8740f2d19b5b2f68
psmacabulos <patrick.macabulos@gmail.com>
2026-04-08 00:37:22 +1000

    fix: fit everything in 4 lines

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.ctr += 1
-    return ', '.join(['BestSchool'] * magic_string.ctr)
+    magic_string.ctr += 1; return ', '.join(['BestSchool'] * magic_string.ctr)
 magic_string.ctr = 0
