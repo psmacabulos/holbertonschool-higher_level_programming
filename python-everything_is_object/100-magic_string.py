b137562833ab4b9e18bd9c4eb8251d1992c1f083
psmacabulos <patrick.macabulos@gmail.com>
2026-04-08 00:51:49 +1000

    fix: return to working state

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-<<<<<<< HEAD
-    magic_string.ctr += 1
-    return ', '.join(['BestSchool'] * magic_string.ctr)
+    magic_string.ctr += 1; return ', '.join(['BestSchool'] * magic_string.ctr)
 magic_string.ctr = 0
-
->>>>>>> 71fb2998aa3738679bcb25ad3c14756cad50f883
